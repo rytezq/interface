90a1b46cd80abe20c17ca2894db1e11643f9a45d
232417 <232417@DESKTOP-10MIQUT>
2026-03-23 16:45:58 +0300

    Добавьте файлы проекта.

---
+++ interface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guitar guitar = new Guitar();
+            guitar.DoSmth();
+            guitar.Play();
+            Piano piano = new Piano();
+            piano.Play();
+        }
+    }
+    interface IPlayable
+    {
+        void Play();
+
+    }
+    class Guitar : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("трунь-трунь");
+        }
+        public void DoSmth()
+        {
+            Console.WriteLine("делать чтото");
+        }
+    }
+    class Piano : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("до-ре-ми");
+        }
+    }
+    class Bayan : IPlayable
+    {
+        public void Test() {
+    }
+}
---
+++ interface2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        interface IAnimal
+        {
+            void Voice();
+        }
+        class Cat : IAnimal
+        {
+            public void Voice()
+            {
+                Console.WriteLine("мяу-мяу");
+            }
+            public void LivesCount()
+            {
+                Console.WriteLine("Введите какую жизнь живет  котяра");
+                int lives = Convert.ToInt32(Console.ReadLine());
+                if (lives >= 1 && lives <= 8)
+                {
+                    Console.WriteLine($"Ваш котяра может прожить еще {9 - lives}");
+                }
+                else if (lives == 9)
+                {
+                    Console.WriteLine("Котяра проживает свою последнюю жизнь");
+                }
+                else
+                {
+                    Console.WriteLine("Ваш котяра умер(((((");
+                }
+            }
+            public void EatNormDay(string gender, int age, int weight, bool breed)
+            {
+               
+            }
+        }
+        class Dog : IAnimal
+        {
+            public void Voice()
+            {
+                Console.WriteLine("гав-гав");
+            }
+        }
+        class Parrot : IAnimal
+        {
+            public void Voice()
+            {
+                Console.WriteLine("СВОБОДУ ПОПУГАЯМ");
+            }
+        }
+    }
+}
